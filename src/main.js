fb6266e3e54513459834d6ff2e936a1cbcd6c305
Carl Xiong <xiongc05@gmail.com>
2015-10-26 13:12:35 +0800

    Implement user and get-script

--- src/main.js
+++ src/main.js
@@ -12,7 +12,38 @@ $(function(){
   console.log("Windows 7/8 Allowed Time Table loaded!");
 
   // * Controls
-  $('#reset-grid').click(function(){
+  function getPrefixStr(){
+    return 'net user '
+      + ( $('#username').val() || "UserName" )
+      + ' /time:';
+  }
+
+  $('button#reset-grid').click(function(){
     timeGrid.reset();
+    $('textarea#script').val(getPrefixStr());
+  });
+
+  $('button#get-script').click(function(){
+    var timeStr = timeGrid.gridData.serialize(),
+        prefixStr = getPrefixStr(),
+        res = "";
+
+    if(!timeStr){
+      res = prefixStr;
+    }
+    else {
+      res = prefixStr + '^' + '\n'
+        + timeStr.split(';').join(';^\n');
+    }
+
+    $('textarea#script').val(res);
+  });
+
+  $('button#edit').one('click', function(e){
+    $(this)
+      .text("Editing...")
+      .attr('disabled', true);
+
+    $('textarea#script').removeAttr('readonly');
   });
 });
