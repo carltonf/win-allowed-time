4b652e8c768465ee4c27f56af13d5d5e275588ed
Carl Xiong <xiongc05@gmail.com>
2015-10-29 16:45:17 +0800

    Clean up for project page

--- src/commons.js
+++
@@ -1,5 +0,0 @@
-// * Constants
-exports.WEEKDAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
-// see /times in https://technet.microsoft.com/en-us/library/bb490718.aspx
-exports.WEEKDAY_ABBRS = ['M', 'T', 'W', 'Th', 'F', 'Sa', 'Su'];
